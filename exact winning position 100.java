b8be0aaf35a220d5560895ec1017680442cbe26a
pranav <pranavgupta401@gmail.com>
2022-04-11 22:44:04 +0530

    adding:ensure the player gets to exact winning position 100

---
+++ exact winning position 100.java
@@ -0,0 +1,51 @@
+public class SnakeLadder {
+	
+	public static int rollDice() {
+		int dice = (int) (Math.random() * 10) % 6 + 1;
+		return dice;
+	}
+
+
+	public static void playerOption() {
+		int position = 0;
+
+		while (position < 100) {
+			int check = (int) (Math.random() * 10) % 3 + 1;
+			System.out.println();
+			System.out.println("Playing Option :" + check);
+
+			switch (check) {
+			case 1:
+				System.out.println("player is not playing ");
+				System.out.println("Player remain at position : " + position);
+				break;
+
+			case 2:
+				int dice = rollDice();
+				System.out.println("Dice Rolled... " + dice);
+				position = position + dice;
+				System.out.println("Its Ladder....");
+				if (position > 100) {
+					position = position - dice;
+				}
+				System.out.println("player is playing at : " + position);
+				break;
+
+			default:
+				int dice2 = rollDice();
+				System.out.println("Dice Rolled.... " + dice2);
+				position = position - dice2;
+				System.out.println("Ohhhh....its Snake");
+				if (position < 1) {
+					position = 0;
+				}
+				System.out.println("player position : " + position);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Lets play...");
+		playerOption();
+	}
+}
